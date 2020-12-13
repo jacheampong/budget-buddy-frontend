1ab3fa85ff029eabf0532059fc31d2775560d655
jacheampong <jacheampong@gmail.com>
2020-12-13 13:13:07 -0500

    Adding Budgets component

---
+++ src/components/budgets/Budgets.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+export default class Budgets extends Component { 
+  render() {
+    return (
+      <div className="App">
+        <h1>All Budgets !</h1>
+      </div>
+    )
+  }
+}
