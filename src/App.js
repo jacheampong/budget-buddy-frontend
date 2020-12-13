08b83ef5e8f8bee8ad7de07355f9037570c5571e
jacheampong <jacheampong@gmail.com>
2020-12-13 13:37:26 -0500

    Adding App js update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import Budgets from './components/budgets/Budgets'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component { 
+  render() {
+    return (
+      <div className="App">
+        <h1>React Budget Buddy!</h1>
+
+        <Budgets />
+      </div>
+    )
+  }
 }
 
-export default App;
