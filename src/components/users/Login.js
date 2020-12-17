5f6add0c9f5ee06eade800e84fee32711e9010a6
jacheampong <jacheampong@gmail.com>
2020-12-16 22:22:06 -0500

    Added materializecss Card, row to Login component

--- src/components/users/Login.js
+++ src/components/users/Login.js
@@ -1,19 +1,49 @@
 import { Component } from "react";
+import { Card, Row, Col } from 'react-materialize';
+import { Link } from 'react-router-dom'
 
 export default class Login extends Component {
     render() {
         return(
             <div className="login-form">
-                <h2>Login to Buddy Account!</h2>
-                <form onSubmit={this.props.login} >
-                <label htmlFor="name">User Name:</label>
-                <input type="text" name="username" required/>
-                <br />
-                <label htmlFor="password">Password:</label>
-                <input type="password" name="password" />
-                <br />
-                <input type="submit" value="Submit" onSubmit={this.props.login} ></input>
-                </form>
+                <Row class="container">
+                    <Col m={6} s={12} >
+                        <Card
+                            className="#80cbc4 teal lighten-4"
+                            textClassName="blue-text text-darken-2"
+                            title=" Login "
+                        >
+                            <div class="row">
+                                <form class="col s12" onSubmit={this.props.login} >
+                                <div class="row">
+                                    <div class="input-field col s10">
+                                    <i class="material-icons prefix">account_circle</i>
+                                    <input id="icon_prefix" type="text" name="username" class="validate" />
+                                    <label for="icon_prefix">User Name</label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="input-field col s10">
+                                    <i class="material-icons prefix">password</i>
+                                    <input id="password" type="password" name="password" class="validate" />
+                                    <label for="password">Password</label>
+                                    </div>
+                                </div>
+                                <button 
+                                    class="btn waves-effect waves-light" 
+                                    type="submit" name="action" 
+                                    onSubmit={this.props.login} 
+                                    >
+                                        Login In <i class="material-icons right">send</i>
+                                </button>
+                                </form>
+
+                                <Link to={`/`}>HOME</Link>
+                            </div>
+                            
+                        </Card>
+                    </Col>
+                </Row>
             </div>
         )
     }
